0645c04b8f61046a4796b2ddd81e599cc977ff79
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-12-10 13:44:55 -0300

    feat: add OrderController

---
+++ server/src/app/controllers/OrderController.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from 'express';
+import { io } from '../../server';
+
+import OrderCategory from '../repositories/OrderCategory';
+
+class OrderController {
+  async listOrders(_request: Request, response: Response) {
+    try {
+      const orders = await OrderCategory.findAll();
+      return response.json(orders);
+    } catch (error) {
+      console.error(error);
+      return response.sendStatus(500);
+    }
+  }
+
+  async createOrder(request: Request, response: Response) {
+    try {
+      const { products, table } = request.body;
+      const order = await OrderCategory.create(products, table);
+      io.emit('orders@new', order);
+      return response.status(201).json(order);
+    } catch (error) {
+      console.error(error);
+      return response.sendStatus(500);
+    }
+  }
+
+  async changeOrderStatus(request: Request, response: Response) {
+    try {
+      const { orderId } = request.params;
+      const { status } = request.body;
+      if (!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
+        return response.status(400).json({
+          error: 'Status should be one of these: WAITING, IN_PRODUCTION, DONE',
+        });
+      }
+      await OrderCategory.update(orderId, status);
+      return response.sendStatus(204);
+    } catch (error) {
+      console.error(error);
+      return response.sendStatus(500);
+    }
+  }
+
+  async cancelOrder(request: Request, response: Response) {
+    try {
+      const { orderId } = request.params;
+      await OrderCategory.delete(orderId);
+      return response.sendStatus(204);
+    } catch (error) {
+      console.error(error);
+      return response.sendStatus(500);
+    }
+  }
+}
+
+export default new OrderController();
