44db93fb986b8f5bf5d4a1e93e42440b9855a335
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-12-12 16:20:00 -0300

    feat: add orders board component

---
+++ web-client/src/components/OrdersBoard/index.tsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
+import { Order } from '../../@types/Order';
+import { api } from '../../utils/api';
+import { OrderModal } from '../OrderModal';
+import { Board, OrdersContainer } from './styles';
+
+interface OrdersBoardProps {
+  icon: string;
+  onCancelOrder: (orderId: string) => void;
+  onChangeOrderStatus: (orderId: string, status: Order['status']) => void;
+  orders: Order[];
+  title: string;
+}
+
+export function OrdersBoard({
+  icon,
+  onCancelOrder,
+  onChangeOrderStatus,
+  orders,
+  title,
+}: OrdersBoardProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState<null | Order>(null);
+
+  function handleOpenModal(order: Order) {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  }
+
+  function handleCloseModal() {
+    setIsModalVisible(false);
+    setSelectedOrder(null);
+  }
+
+  async function handleChangeOrderStatus() {
+    if (!selectedOrder) return;
+    setIsLoading(true);
+    const status =
+      selectedOrder.status === 'WAITING' ? 'IN_PRODUCTION' : 'DONE';
+    await api.patch(`/orders/${selectedOrder._id}`, { status });
+    toast.success(
+      `The order from table ${selectedOrder.table} has its status changed!`
+    );
+    onChangeOrderStatus(selectedOrder._id, status);
+    setIsLoading(false);
+    setIsModalVisible(false);
+  }
+
+  async function handleCancelOrder() {
+    if (!selectedOrder) return;
+    setIsLoading(true);
+    await api.delete(`/orders/${selectedOrder._id}`);
+    toast.info(`The order from table ${selectedOrder.table} was canceled!`);
+    onCancelOrder(selectedOrder._id);
+    setIsLoading(false);
+    setIsModalVisible(false);
+  }
+
+  return (
+    <Board>
+      <OrderModal
+        onClose={handleCloseModal}
+        order={selectedOrder}
+        visible={isModalVisible}
+        onCancelOrder={handleCancelOrder}
+        onChangeOrderStatus={handleChangeOrderStatus}
+        isLoading={isLoading}
+      />
+      <header>
+        <span>{icon}</span>
+        <strong>{title}</strong>
+        <span>({orders.length})</span>
+      </header>
+      {orders.length > 0 && (
+        <OrdersContainer>
+          {orders.map(order => (
+            <button
+              type="button"
+              key={order._id}
+              onClick={() => handleOpenModal(order)}
+            >
+              <strong>Table {order.table}</strong>
+              <span>{order.products.length} itens</span>
+            </button>
+          ))}
+        </OrdersContainer>
+      )}
+    </Board>
+  );
+}
