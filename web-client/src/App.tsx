f0d4a8304cbed8162a6ca0c717a72540612211ed
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-12-12 16:20:46 -0300

    feat: add global styles and axios api

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,12 +1,17 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { Header } from './components/Header';
+import { Orders } from './components/Orders';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 export function App() {
   return (
     <>
+      <ToastContainer position="bottom-center" />
       <GlobalStyles />
+      <Header />
+      <Orders />
     </>
   );
 }
