22c4e71949fa5bbb88805de63e5af5b344ec7f5b
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2023-02-07 09:49:41 -0300

    Feat: Change primary color

--- mobile/src/components/Button/styles.ts
+++ mobile/src/components/Button/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
-  background-color: ${({ disabled }) => (disabled ? '#999999' : '#d73035;')};
+  background-color: ${({ disabled }) => (disabled ? '#999999' : '#6b1114;')};
   border-radius: 48px;
   padding: 14px 24px;
 `;
